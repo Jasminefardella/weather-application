9c8723ae277fe2c6b45e9343d74f413aab6ecfee
Jasminefardella <jasmine.fardella@hotmail.com>
2023-11-01 08:30:25 +0800

    Update javascript weather description

--- src/script.js
+++ src/script.js
@@ -58,7 +58,7 @@ function displayWeatherCondition(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
 }
 
 function searchCity(city) {
